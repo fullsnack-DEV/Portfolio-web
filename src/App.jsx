79fafd44ae133238a63dfa5f3d8d14acdadc5b8d
pratik <pratikpatre2019@gmail.com>
2022-03-15 18:14:07 +0530

    Added TailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-2xl  bg-red-300">Hello From TailWind</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
@@ -15,10 +16,13 @@ function App() {
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
+        <p className="bg-red-600 font-extrabold text-5xl ">
+          Edit <code>App.jsx</code> and save to test HMR updates. oye
         </p>
         <p>
+          <p className="text-4xl bg-cyan-300 uppercase mt-24 align-baseline">
+            HEllo
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
