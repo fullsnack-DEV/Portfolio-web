d0843c8a72788d908340b7f630e80320fdc88de6
pratik <pratikpatre2019@gmail.com>
2022-03-17 17:45:00 +0530

    Added the Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-2xl  bg-red-300">Hello From TailWind</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p className="bg-red-600 font-extrabold text-5xl ">
-          Edit <code>App.jsx</code> and save to test HMR updates. oye
-        </p>
-        <p>
-          <p className="text-4xl bg-cyan-300 uppercase mt-24 align-baseline">
-            HEllo
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs hai ye
-          </a>
-        </p>
-      </header>
+      <HomePage />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackbackground: "#1B1A17",
+        gradred: "#d65bca",
+        gradredark: "#21d190",
+      },
+      fontFamily: {
+        Fredoka: "Fredoka",
+      },
+
+      lineHeight: {
+        "extra-loose": "1.2",
+      },
+    },
   },
   plugins: [],
 };
